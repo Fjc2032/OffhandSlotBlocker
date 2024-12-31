00868cfab828807d0cace9ecc4d880a7865122e5
Fjc <charlesfrancoos@gmail.com>
2024-12-31 02:40:28 -0500

    Fix material check and build against JDK21

--- src/main/java/me/fjc/offhandslotblocker/InvListener.java
+++ src/main/java/me/fjc/offhandslotblocker/InvListener.java
@@ -6,29 +6,28 @@ import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryInteractEvent;
 import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.ItemStack;
 
-public abstract class InvListener extends Event implements Listener {
+
+public class InvListener extends OffhandSlotBlocker implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
-    //Method 1: Cancelling the click event entirely. This will block ALL pig spawn eggs,
-    //regardless if it is MythicMobs or not
-    public void onClick(InventoryClickEvent event) {
-        //Check if the slot being clicked is the offhand slot. If not, exit.
+    public void onClickCraftInv(InventoryClickEvent event) {
         if (!event.getInventory().getType().equals(InventoryType.CRAFTING) && event.getRawSlot() == 45) {
             return;
         } else {
-            //Declare Bukkit Player as event.getWhoClicked();
             Player player = (Player) event.getWhoClicked();
+            ItemStack target = event.getCurrentItem();
 
             //Checks if the current item is a pig spawn egg and if the target is a player
-            if (event.getCurrentItem().equals(Material.PIG_SPAWN_EGG) && event.getWhoClicked().equals(player)) {
+            if (target != null && target.getType() == (Material.PIG_SPAWN_EGG) && event.getWhoClicked().equals(player)) {
                 event.setCancelled(true);
                 player.sendMessage("Don't put this into your offhand!");
             }
@@ -37,10 +36,24 @@ public abstract class InvListener extends Event implements Listener {
         }
 
     }
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onClickPlayerInv(InventoryClickEvent event) {
+        if (!(event.getWhoClicked() instanceof Player)) return;
+
+        Player player = (Player) event.getWhoClicked();
+        ItemStack target = event.getCurrentItem();
+
+        if (target != null && target.getType() == (Material.PIG_SPAWN_EGG) && event.getWhoClicked().equals(player)) {
+            if (event.getSlot() == 40) {
+                event.setCancelled(true);
+                player.sendMessage("Don't put this into your offhand!");
+            }
+
+        }
+    }
+    /*
     @EventHandler(priority = EventPriority.HIGH)
-    //Method 2: Check if the mob is a mythic mob first, then cancel the spawn event if there
-    //is an egg in the offhand slot. Pretty hacky.
-    public void onSpawn(CreatureSpawnEvent event) {
+    public void onSpawn(CreatureSpawnEvent event, InventoryInteractEvent interactEvent) {
         //Declares bukkitEntity
         Entity bukkitEntity = event.getEntity();
 
@@ -49,7 +62,7 @@ public abstract class InvListener extends Event implements Listener {
         if(isMythicMob) {
 
             //Checks if the mob in question was spawned by MythicMobs
-            if (event.getSpawnReason().equals(CreatureSpawnEvent.SpawnReason.CUSTOM) && event.getEntityType().equals(EntityType.PIG)) {
+            if (event.getSpawnReason().equals(CreatureSpawnEvent.SpawnReason.EGG) && event.getEntityType().equals(EntityType.PIG)) {
                 event.setCancelled(true);
 
                 //Send a warning message to the player (this might not work since getEntityType() will never equal PLAYER)
@@ -62,6 +75,7 @@ public abstract class InvListener extends Event implements Listener {
 
         }
     }
+    */
 
 
 
--- src/main/java/me/fjc/offhandslotblocker/OffhandSlotBlocker.java
+++ src/main/java/me/fjc/offhandslotblocker/OffhandSlotBlocker.java
@@ -1,8 +1,14 @@
 package me.fjc.offhandslotblocker;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class OffhandSlotBlocker extends JavaPlugin implements Listener {
@@ -17,12 +23,28 @@ public class OffhandSlotBlocker extends JavaPlugin implements Listener {
             Bukkit.getLogger().warning("Registering events anyway.");
             getServer().getPluginManager().registerEvents(this, this);
         }
-        saveDefaultConfig();
-        saveConfig();
+        getLogger().info("Loading configuration..");
+        createConfig();
+        getLogger().info("Success.");
+
+        getServer().getPluginManager().registerEvents(new InvListener(), this);
         Bukkit.getLogger().info("Everything has loaded, probably.");
     }
     public void onDisable() {
-        Bukkit.getLogger().info("Shutting down.");
+        getLogger().info("Shutting down.");
         saveConfig();
+    }
+    private void createConfig() {
+        File file = new File(getDataFolder(), "config.yml");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        FileConfiguration configuration = YamlConfiguration.loadConfiguration(file);
+        configuration.addDefault("Target", Material.PIG_SPAWN_EGG);
+
     }
 }
